2ce86a1cd54a5cc97bd77fb65c64cbc8ef8aaa46
vibhuk10 <vibhuk10@gmail.com>
2025-08-02 19:48:04 -0700

    Updates

---
+++ precache-manifest.fabe639a88330606810dbf19d725f16a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d111b0a7dc0623633e7f50c6fc814630",
+    "url": "/fantasy-rankings/index.html"
+  },
+  {
+    "revision": "2cd4e4249f8956e6b1fe",
+    "url": "/fantasy-rankings/static/css/main.0e6dea33.chunk.css"
+  },
+  {
+    "revision": "f5e22056801de03f872b",
+    "url": "/fantasy-rankings/static/js/2.d5a6f219.chunk.js"
+  },
+  {
+    "revision": "1759db8a9c8cbedde8e933d54cd4bcbc",
+    "url": "/fantasy-rankings/static/js/2.d5a6f219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7501235e2a215c249c6",
+    "url": "/fantasy-rankings/static/js/3.5c02bbdd.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/fantasy-rankings/static/js/3.5c02bbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7bb545daf964cb0479",
+    "url": "/fantasy-rankings/static/js/4.1395beb9.chunk.js"
+  },
+  {
+    "revision": "036dc6738e2543acd8cf9b1a224d0765",
+    "url": "/fantasy-rankings/static/js/4.1395beb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd4e4249f8956e6b1fe",
+    "url": "/fantasy-rankings/static/js/main.90add9f3.chunk.js"
+  },
+  {
+    "revision": "692dace786ceb2bab782",
+    "url": "/fantasy-rankings/static/js/runtime-main.edab0a77.js"
+  }
+]);
